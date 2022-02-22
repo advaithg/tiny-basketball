5f65ba22ebab394147e8b1d13ba7a1daa7ac93ea
advaithg <gowrishettyadvaith@gmail.com>
2022-02-21 22:52:19 -0800

    adds wall

--- tiny-basketball.js
+++ tiny-basketball.js
@@ -32,18 +32,34 @@ export class TinyBasketball extends Scene {
       basketball: new defs.Subdivision_Sphere(10),
       basketball_stripe: new defs.Torus(1, 30),
       ground: new defs.Square(),
-      wall: new defs.Square(),
+      wall1: new defs.Square(),
+      wall2: new defs.Square(),
       backboard_hoop: new defs.Torus(),
       backboard: new defs.Square(),
     };
 
+    this.shapes.wall2.arrays.texture_coord = []
+        this.shapes.wall1.arrays.texture_coord.forEach( (x, i) => {
+            const next = new Vector(this.shapes.wall1.arrays.texture_coord[i]);
+            next.scale_by(11);
+            this.shapes.wall2.arrays.texture_coord.push(next);
+        });
+    
     // Materials
     this.materials = {
+      phong: new Material(new defs.Textured_Phong(), {
+        color: hex_color("#ffffff"),
+      }),
       basketball: new Material(new defs.Textured_Phong(), {
         color: hex_color("#F88158"),
       }),
       basketball_stripe: new Material(new defs.Textured_Phong(), {
         color: hex_color("#000000"),
+      }),
+      wall_texture: new Material(new defs.Textured_Phong(), {
+        color: hex_color("#000000"),
+        ambient: 1,
+        texture: new Texture("assets/brick-wall.jpeg"),
       }),
     };
 
@@ -68,7 +84,7 @@ export class TinyBasketball extends Scene {
       // Define the global camera and projection matrices, which are stored in program_state.
       program_state.set_camera(Mat4.translation(0, 0, -8));
     }
-    
+
     program_state.projection_transform = Mat4.perspective(
       Math.PI / 4,
       context.width / context.height,
@@ -118,18 +134,23 @@ export class TinyBasketball extends Scene {
     // WALL
     let wall_transform = Mat4.identity();
     wall_transform = wall_transform.times(Mat4.translation(0, 0, -10));
-    wall_transform = wall_transform.times(Mat4.scale(10, 10, 0.1));
-    this.shapes.wall.draw(
+    wall_transform = wall_transform.times(Mat4.scale(14, 10, 0.1));
+    this.shapes.wall2.draw(
       context,
       program_state,
       wall_transform,
-      this.materials.phong.override({ color: hex_color("#ffffff") })
+      this.materials.wall_texture,
     );
 
     // GROUND
 
     // BACKBOARD
     let backboard_transform = Mat4.identity();
-    this.shapes.backboard.draw(context, program_state, backboard_transform, this.materials.phong);
+    this.shapes.backboard.draw(
+      context,
+      program_state,
+      backboard_transform,
+      this.materials.phong
+    );
   }
 }
