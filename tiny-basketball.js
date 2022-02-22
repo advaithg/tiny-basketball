9da62dbb59e4399ddf2305b8d294bfcbb7c916fa
Dylon Tjanaka <dylon.tjanaka@gmail.com>
2022-02-22 00:34:30 -0800

    hehe

--- tiny-basketball.js
+++ tiny-basketball.js
@@ -29,8 +29,12 @@ export class TinyBasketball extends Scene {
 
     // Shapes
     this.shapes = {
-      basketball: new defs.Subdivision_Sphere(10),
-      basketball_stripe: new defs.Torus(1, 30),
+      basketball: new defs.Subdivision_Sphere(4),
+      basketball_stripe: new defs.Surface_Of_Revolution(
+        20,
+        20,
+        Vector3.cast([0, 1, 1], [0, 1, -1])
+      ),
       ground: new defs.Square(),
       wall: new defs.Square(),
       backboard_hoop: new defs.Torus(),
@@ -39,10 +43,13 @@ export class TinyBasketball extends Scene {
 
     // Materials
     this.materials = {
-      basketball: new Material(new defs.Textured_Phong(), {
+      phong: new Material(new defs.Textured_Phong(), {
+        color: hex_color("#FFFFFF"),
+      }),
+      basketball: new Material(new defs.Phong_Shader(), {
         color: hex_color("#F88158"),
       }),
-      basketball_stripe: new Material(new defs.Textured_Phong(), {
+      basketball_stripe: new Material(new defs.Phong_Shader(), {
         color: hex_color("#000000"),
       }),
     };
@@ -54,11 +61,11 @@ export class TinyBasketball extends Scene {
       vec3(0, 0, 0),
       vec3(0, 1, 0)
     );
+
+    console.log("Sus");
   }
 
-  make_control_panel() {
-    // TODO: Control panel
-  }
+  make_control_panel() {}
 
   display(context, program_state) {
     if (!context.scratchpad.controls) {
@@ -68,7 +75,7 @@ export class TinyBasketball extends Scene {
       // Define the global camera and projection matrices, which are stored in program_state.
       program_state.set_camera(Mat4.translation(0, 0, -8));
     }
-    
+
     program_state.projection_transform = Mat4.perspective(
       Math.PI / 4,
       context.width / context.height,
@@ -76,7 +83,7 @@ export class TinyBasketball extends Scene {
       100
     );
 
-    const light_position = vec4(0, 10, 0, 1);
+    const light_position = vec4(10, 10, 10, 1);
     program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
 
     this.dt = program_state.animation_delta_time / 1000;
@@ -93,28 +100,27 @@ export class TinyBasketball extends Scene {
       this.materials.basketball
     );
 
-    model_transform = Mat4.identity();
-    model_transform = model_transform.times(Mat4.scale(1.5, 1.5, 0.1));
-    this.shapes.basketball_stripe.draw(
-      context,
-      program_state,
-      model_transform,
-      this.materials.basketball_stripe
-    );
+    let stripe_matrices = [
+      Mat4.rotation(Math.PI / 2, 0, 1, 0).times(Mat4.scale(1.02, 1.02, 0.03)),
+      Mat4.rotation(Math.PI / 2, 1, 0, 0).times(Mat4.scale(1.02, 1.02, 0.03)),
+      Mat4.rotation(Math.PI / 4, 0, 0, 1)
+        .times(Mat4.rotation(Math.PI / 2, 0, 1, 0))
+        .times(Mat4.scale(1.02, 1.02, 0.03)),
+      Mat4.rotation(-Math.PI / 4, 0, 0, 1)
+        .times(Mat4.rotation(Math.PI / 2, 0, 1, 0))
+        .times(Mat4.scale(1.02, 1.02, 0.03)),
+    ];
 
-    model_transform = Mat4.identity();
-    model_transform = model_transform.times(
-      Mat4.rotation(Math.PI / 2, 0, 1, 0)
-    );
-    model_transform = model_transform.times(Mat4.scale(1.5, 1.5, 0.1));
-    this.shapes.basketball_stripe.draw(
-      context,
-      program_state,
-      model_transform,
+    for (let i = 0; i < stripe_matrices.length; i++) {
+      this.shapes.basketball_stripe.draw(
+        context,
+        program_state,
+        stripe_matrices[i],
+        this.materials.basketball_stripe
+      );
+    }
 
-      this.materials.phong.override({ color: hex_color("#000000") })
-    );
-
+    /*
     // WALL
     let wall_transform = Mat4.identity();
     wall_transform = wall_transform.times(Mat4.translation(0, 0, -10));
@@ -130,6 +136,12 @@ export class TinyBasketball extends Scene {
 
     // BACKBOARD
     let backboard_transform = Mat4.identity();
-    this.shapes.backboard.draw(context, program_state, backboard_transform, this.materials.phong);
+    this.shapes.backboard.draw(
+      context,
+      program_state,
+      backboard_transform,
+      this.materials.phong
+    );
+    */
   }
 }
