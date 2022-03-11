b37492578b426e21dffc0a511f3d8b206fe96ef2
Dylon Tjanaka <dylon.tjanaka@gmail.com>
2022-03-11 02:48:57 -0800

    remove reflective phong class lol

--- tiny-basketball.js
+++ tiny-basketball.js
@@ -718,24 +718,3 @@ export class TinyBasketball extends Scene {
     }
   }
 }
-
-class Reflective_Phong extends defs.Textured_Phong {
-  fragment_glsl_code() {
-    return (
-      this.shared_glsl_code() +
-      `
-          varying vec2 f_tex_coord;
-          uniform sampler2D texture;
-          uniform float animation_time;
-          void main(){
-              // Sample the texture image in the correct place:
-              vec4 tex_color = texture2D( texture, f_tex_coord );
-              if( tex_color.w < .01 ) discard;
-                                                                       // Compute an initial (ambient) color:
-              gl_FragColor = vec4( ( tex_color.xyz + shape_color.xyz ) * ambient, shape_color.w * tex_color.w ); 
-                                                                       // Compute the final color with contributions from lights:
-              gl_FragColor.xyz += phong_model_lights( normalize( N ), vertex_worldspace );
-      } `
-    );
-  }
-}
